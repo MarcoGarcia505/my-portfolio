ff39caafa97a127b7d1b25ae3f515a68ee1159e1
MarcoGarcia505 <markopolo505@outlook.com>
2023-09-24 16:41:05 -0600

    add use client

--- components/experience/Experience.jsx
+++ components/experience/Experience.jsx
@@ -1,3 +1,4 @@
+'use client'
 import React from 'react'
 import styles from './experience.module.css'
 import { CardExperience } from '..'
--- components/porjects/Projects.jsx
+++ components/porjects/Projects.jsx
@@ -1,3 +1,4 @@
+'use client'
 import React from 'react'
 import styles from './projects.module.css'
 import surtidora from '../../public/img/img_proyectos/surtidora.png'
