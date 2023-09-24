ccd27c5cc3b38abbe72ba0fdf6614e45898dd6a1
MarcoGarcia505 <markopolo505@outlook.com>
2023-09-24 16:53:57 -0600

    update page

--- app/page.js
+++ app/page.js
@@ -1,7 +1,6 @@
 import Image from 'next/image'
 import styles from './page.module.css'
-import { About, Experience, Footer, Header, Intro, Projects } from '../components'
-import { Main } from '@/components'
+import { About, Experience, Footer, Header, Intro, Projects, Main } from '../components/index'
 
 export default function Home() {
   return (
